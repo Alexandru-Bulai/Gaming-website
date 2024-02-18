75eacaf2e38f84b0ff2ebf81861f2680359e2a2b
Alex <alexandru.bulai1@proton.me>
2024-02-19 00:07:42 +0200

    make vite.config.js more simple

--- gaming-website/vite.config.js
+++ gaming-website/vite.config.js
@@ -1,13 +1,5 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import fs from 'fs';
-
-// Utility function to copy index.html from src to public
-const copyIndexToPublic = () => {
-	const srcIndexPath = resolve(__dirname, 'src/index.html');
-	const publicIndexPath = resolve(__dirname, 'public/index.html');
-	fs.copyFileSync(srcIndexPath, publicIndexPath);
-};
 
 export default defineConfig({
 	root: 'src',
@@ -16,13 +8,10 @@ export default defineConfig({
 		emptyOutDir: true,
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, 'src/index.html')
+				main: resolve(__dirname, 'src/index.html'),
 			},
 			// Add other Rollup options if necessary
-		}
+		},
 	},
-	plugins: [{
-		name: 'copy-index-html',
-		buildStart: copyIndexToPublic
-	}]
+	// ... other configurations
 });
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./public/**/*.html', './src/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
