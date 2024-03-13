3f0c42c5f3f3a330d843392f8092e0f07cf4dc1e
Alex <alexandru.bulai1@proton.me>
2024-03-13 21:54:18 +0200

    lint fix

--- src/test/indexDom.test.js
+++ src/test/indexDom.test.js
@@ -3,37 +3,37 @@
  * @param {boolean} sub - If true, decrements the pageCount by 1.
  */
 
-let pageCount = 0;
+let pageCount = 0
 
-function moveMenu(add, sub) {
+function moveMenu (add, sub) {
   if (add) {
-    pageCount += 1;
+    pageCount += 1
   }
   if (sub) {
-    pageCount -= 1;
+    pageCount -= 1
   }
   if (pageCount < 0) {
-    pageCount = 0;
+    pageCount = 0
   }
 }
 
-describe("Highlight menu count", () => {
-  test("Check if count starts at 0", () => {
-    expect(pageCount).toBe(0);
-  });
+describe('Highlight menu count', () => {
+  test('Check if count starts at 0', () => {
+    expect(pageCount).toBe(0)
+  })
 
-  test("Check increasing count", () => {
-    moveMenu(true, false);
-    expect(pageCount).toBe(+1);
-  });
+  test('Check increasing count', () => {
+    moveMenu(true, false)
+    expect(pageCount).toBe(+1)
+  })
 
-  test("Check decreaseing count", () => {
-    moveMenu(false, true);
-    expect(pageCount).toBe(0);
-  });
+  test('Check decreaseing count', () => {
+    moveMenu(false, true)
+    expect(pageCount).toBe(0)
+  })
 
-  test("Ensure nothing else can affect count", () => {
-    moveMenu(false, false);
-    expect(pageCount).toBe(0);
-  });
-});
+  test('Ensure nothing else can affect count', () => {
+    moveMenu(false, false)
+    expect(pageCount).toBe(0)
+  })
+})
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
     host: true,
-    port: 3000,
+    port: 3000
   },
   test: {
     globals: true,
-    environment: "happy-dom",
-  },
-});
+    environment: 'happy-dom'
+  }
+})
