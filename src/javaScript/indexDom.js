b42e5513da4975aa0aef5d7a25d64a1590df0601
Alex <alexandru.bulai1@proton.me>
2024-03-21 10:27:27 +0200

    check is JS ignores semicolon

--- src/javaScript/indexDom.js
+++ src/javaScript/indexDom.js
@@ -3,19 +3,19 @@
  * @param {boolean} sub - If true, decrements the pageCount by 1.
  */
 
-export let pageCount = 0
+export let pageCount = 0;
 
-export function moveMenu (add, sub) {
+export function moveMenu(add, sub) {
   if (add) {
-    pageCount += 1
+    pageCount += 1;
   }
   if (sub) {
-    pageCount -= 1
+    pageCount -= 1;
   }
   if (pageCount < 0) {
-    pageCount = 0
+    pageCount = 0;
   }
   if (add && sub) {
-    throw new Error('Please select only one option at a time')
+    throw new Error("Please select only one option at a time");
   }
 }
--- src/test/indexDom.test.js
+++ src/test/indexDom.test.js
@@ -1,28 +1,28 @@
-import { moveMenu, pageCount } from '../javaScript/indexDom'
+import { moveMenu, pageCount } from "../javaScript/indexDom";
 
-describe('Highlight menu count', () => {
-  test('Check if count starts at 0', () => {
-    expect(pageCount).toBe(0)
-  })
+describe("Highlight menu count", () => {
+  test("Check if count starts at 0", () => {
+    expect(pageCount).toBe(0);
+  });
 
-  test('Check increasing count', () => {
-    moveMenu(true, false)
-    expect(pageCount).toBe(+1)
-  })
+  test("Check increasing count", () => {
+    moveMenu(true, false);
+    expect(pageCount).toBe(+1);
+  });
 
-  test('Check decreaseing count', () => {
-    moveMenu(false, true)
-    expect(pageCount).toBe(0)
-  })
+  test("Check decreaseing count", () => {
+    moveMenu(false, true);
+    expect(pageCount).toBe(0);
+  });
 
-  test('Ensure nothing else can affect count', () => {
-    moveMenu(false, false)
-    expect(pageCount).toBe(0)
-  })
+  test("Ensure nothing else can affect count", () => {
+    moveMenu(false, false);
+    expect(pageCount).toBe(0);
+  });
 
-  test('Throw error when both add and sub are true', () => {
+  test("Throw error when both add and sub are true", () => {
     expect(() => moveMenu(true, true)).toThrow(
-      'Please select only one option at a time'
-    )
-  })
-})
+      "Please select only one option at a time",
+    );
+  });
+});
